8b759e5bcdbea253de629819ac87841dd041e2f7
MUHAMMAD UMAR <umarfarhan@proton.me>
2025-08-14 02:24:55 +0500

    Home Page implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import LandingPage from "../components/LandingPage";
+import Tools from "./../components/Tools";
+import Testimonial from "../components/Testimonial";
+import Plans from "../components/Plans";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    
+      <Navbar />
+      <LandingPage />
+      <Tools />
+      <Testimonial />
+      <Plans />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
